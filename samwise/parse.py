bb9dcf145453a12a65a85aad7f8362003a5ffd99
NT <nturner@zetta.ai>
2022-07-05 16:20:11 -0400

    feat: parsemap helper fn for argparse

--- samwise/parse.py
+++ samwise/parse.py
@@ -1,8 +1,11 @@
-"""Functions for parsing command files."""
+"""Functions for parsing various strings of interest."""
 from __future__ import annotations
+
+from typing import Iterable
 
 
 def parsecmd(filename: str) -> list[str]:
+    """Parses a file containing a command-line command."""
     with open(filename) as f:
         lines = f.readlines()
 
@@ -17,3 +20,8 @@ def parsecmd(filename: str) -> list[str]:
                 args.append(arg)
 
     return args
+
+
+def parsemap(mapargs: Iterable[str]) -> dict[str, str]:
+    """Parses an iterable of arguments into a directory mapping."""
+    return dict(arg.split("::") for arg in mapargs)
