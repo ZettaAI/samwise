8ef07177045fb04caebfc43fa6e547812014cb00
NT <nturner@zetta.ai>
2022-08-05 10:09:07 -0400

    test: shorter test for run

--- test/test_run.py
+++ test/test_run.py
@@ -6,12 +6,12 @@ from samwise import run
 
 def test_run(dummy_dir):
     def f():
-        for i in range(5):
+        for i in range(2):
             time.sleep(1)
             print("still alive")
         print("dying")
 
     localdir = "test/dummy_dir_local"
-    run(f, {dummy_dir: localdir}, period=5)
+    run(f, {dummy_dir: localdir}, period=2)
 
     shutil.rmtree(localdir)
